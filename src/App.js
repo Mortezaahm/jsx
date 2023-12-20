4d26554f8dd804039c6f4dbe3057c8fa6ff0c301
Morteza <morteza.ahmadianmanzary@gmail.com>
2023-12-20 09:18:32 +0100

    EDIT

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
-import 'bulma/css/bulma.css'
-import ProfileCard from "./components/ProfileCard";
-import AlexaImage from "./images/alexa.png";
-import CortanaImage from "./images/cortana.png";
-import SiriImage from "./images/siri.png";
-function App() {
+import "bulma/css/bulma.css";
+import ProfileCard from './components/ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
+function App() {
   return (
     <>
       <section className="hero is-primary">
         <div className="hero-body">
-          <p className="title">
-            Personal Digital Assistants
-          </p>
+          <p className="title">Personal Digital Assistant</p>
         </div>
       </section>
-
+     
         <div className="container">
-          <div className="section">
+          <section className="section">
             <div className="columns">
               <div className="column is-4">
-                <ProfileCard 
-                  title="Alexa" 
-                  handle="@alexa99" 
+                <ProfileCard
+                  title="Alexa"
+                  handle="@alexa99"
                   image={AlexaImage}
-                  description="Alexa was created by Amazon and help you buy things."
+                  description="Alexa is a virtual assistant developed by Amazon, first used in the Amazon Echo smart speakers developed by Amazon Lab126. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news."
                 />
               </div>
               <div className="column is-4">
-                <ProfileCard 
-                  title="Cortana" 
-                  handle="@cortana32" 
-                  image={CortanaImage}
-                  description="Cortana was created by Microsoft and help you manage things."
-                />
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana is a virtual assistant developed by Samsung, first used in the Amazon Echo smart speakers developed by Amazon Lab126. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news."
+              />
               </div>
               <div className="column is-4">
-                <ProfileCard 
-                  title="Siri" 
-                  handle="@siri01" 
-                  image={SiriImage}
-                  description="Siri was created by Apple and help you manage apps."
-                />
+              <ProfileCard
+                title="Siri"
+                handle="@Siri01"
+                image={SiriImage}
+                description="Siri is a virtual assistant developed by Apple, first used in the Amazon Echo smart speakers developed by Amazon Lab126. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news."
+              />
               </div>
             </div>
-          </div>
+          </section>
         </div>
-
-      
     </>
   );
 }
